de6ea614169e4ef91ff78509638d70a8594c8414
Yunnii <yunniihi@gmail.com>
2011-01-18 08:55:13 -0800

    ==_

--- affin/hellolong.c
+++ affin/hellolong.c
@@ -7,9 +7,11 @@ int main()
 	int a[1000][1000];
 	int summator=0;
 	clock_t s1=clock();
-	for(int k=0;k<size;k++)
-		for(int i=0;i<size;i++)
-			for(int j=0;j<size;j++)
-				summator+=a[i][j];
+	int k,i,j;
+	for(k=0;k<size;k++)
+		for(i=0;i<size;i++)
+			for(j=0;j<size;j++)
+				//summator+=a[i][j];
+				a[i][j]=0;
 		return 0;
 }
