bdd43fb7dc461ca2bc5f3009bfcd6845e4ad5f98
Yunnii <yunniihi@gmail.com>
2011-01-18 14:40:48 +0500

    task on affin

---
+++ affin/hellolong.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<time.h>
+
+int main()
+{
+	int size = 1000;
+	int a[size][size];
+	int summator=0;
+	clock_t s1=clock();
+	for(int k=0;k<size;k++)
+		for(int i=0;i<size;i++)
+			for(int j=0;j<size;j++)
+				summator+=a[i][j];
+		return 0;
+}
