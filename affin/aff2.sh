c0fdac79bd5e17f4c521344460b08f4cc3582a5f
Yunnii <yunniihi@gmail.com>
2011-01-18 08:54:46 -0800

    =

--- affin/aff2.sh
+++ affin/aff2.sh
@@ -7,7 +7,7 @@
 	while [ $PIDold = $PIDnew ];do					#
 		echo "x", `ps -a | grep "helloc.out" | awk '{print $1}'`, "x";
 		taskset -p $j $PID > /dev/null				#
-		j=$(($j^3));						#
+	#	j=$(($j^3));						#
 		if [[ $PID!=`ps -a | grep "helloc.out" | awk '{print $1}'` ]];then #
 			break;						#
 		fi							#
