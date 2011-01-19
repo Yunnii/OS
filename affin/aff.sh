08127208858bb3ad5c7c2375413224a9dc47e0a3
Yunnii <yunniihi@gmail.com>
2011-01-19 00:30:58 -0800

    i

--- affin/aff.sh
+++ affin/aff.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-	gcc hellolong.c -o helloc.out							#
-	time ./helloc.out &										#
-	PID=`ps -a | grep "helloc.out" | awk '{print $1}'` 		#
-	j=1;													#
-	while [ `ps -a | grep "helloc.out" | awk '{print $1}'` != ""];do					#
-		taskset -p $j $PID > /dev/null						#
-		j=$(($j^1));										#
-	done													#
-	time ./helloc.out										#
-	rm helloc.out											#
+        gcc hellolong.c -o helloc.out                                                   #
+        time ./helloc.out &                                                                             #
+        PID=`ps -a | grep "helloc.out" | awk '{print $1}'`              #
+        j=1;    #
+        while [  -z `ps -a | grep "helloc.out" | awk '{print $1}'` ]; do                                        #
+                taskset -p $j $PID > /dev/null                                          #
+                j=$(($j^1));                                                                            #
+        done                                                                                                    #
+        time ./helloc.out                                                                               #
+        rm helloc.out                                                                                   #
+
